768ffbdb994c8c39bca7b3fe8e267007c738a9be
ShivaniMehrotra4 <shivanimehrotra.sms@gmail.com>
2020-02-28 13:44:28 +0530

    updated source code

--- src/main/java/com/knoldus/AppDriver.java
+++ src/main/java/com/knoldus/AppDriver.java
@@ -6,14 +6,15 @@ import java.util.List;
 public class AppDriver {
 
     public static void main(String[] args) {
-        //mvn exec:java -Dexec.mainClass=com.knoldus.SampleApp
+        //mvn exec:java -Dexec.mainClass=com.knoldus.AppDriver
         PrintResults printResult = new PrintResults();
 
         //QUESTION-1
         printResult.display();
 
         //QUESTION-2
-        List<String> myList = Arrays.asList("Ahemdabad", "cat", "himachal", "Coimbatore", "Delhi", "Calcutta", "Sahibabad", "Cupid");
+        List<String> myList = Arrays.asList("Ahemdabad", "cat", "himachal", "Coimbatore", "Delhi",
+                "Calcutta", "Sahibabad", "Cupid");
         printResult.filterOutList(myList);
 
         //QUESTION-3
@@ -28,25 +29,16 @@ public class AppDriver {
         User sampleUser4 = new User(1353, "Muskan", "Faridabad", "13");
         User sampleUser5 = new User(1351, "Upanshu", "Delhi", "18");
         User sampleUser6 = new User(1359, "Kavya", "Delhi", "25");
-
-        List<User> listOfUsers = Arrays.asList(sampleUser1, sampleUser2, sampleUser3, sampleUser4, sampleUser5, sampleUser6);
+        List<User> listOfUsers = Arrays.asList(sampleUser1, sampleUser2, sampleUser3,
+                sampleUser4, sampleUser5, sampleUser6);
         User test = new User();
-//        test.listOperations(listOfUsers);
-
         //1
         List<String> result1 = test.questionOne(listOfUsers);
-        //System.out.println("Users with age above 18 and address Delhi : " + result1);
-
         //2
         List<String> result2 = test.questionTwo(listOfUsers);
-        //System.out.println("Users with the id greater than 10 and age less than 20 : " + result2);
-
         //3
         List<String> result3 = test.questionThree(listOfUsers);
-        //System.out.println("Users with age as even number : " + result3);
-
         //4
         List<String> result4 = test.questionFour(listOfUsers);
-        //System.out.println("Users with age equals to 18 and address Delhi : " + result4);
     }
 }
--- src/main/java/com/knoldus/PrintResults.java
+++ src/main/java/com/knoldus/PrintResults.java
@@ -6,23 +6,28 @@ import java.util.stream.IntStream;
 
 import static java.lang.Integer.min;
 
+/**
+ * This class is used for printing results of various queries specified by the user.
+ */
 public class PrintResults {
 
     public void display() {
-        PersonalDetails pd = ((name, age, pinCode, countryCode, city) -> (name + " " + age + " " + pinCode + " " + countryCode + " " + city));
-        //System.out.println("The Details are : " + pd.displayDetails("Shivani", 24, 201005, 91, "Sahibabad"));
+        PersonalDetails pd = ((name, age, pinCode, countryCode, city) ->
+                (name + " " + age + " " + pinCode + " " + countryCode + " " + city));
     }
 
-    public void filterOutList(List<String> myList)
-    {
-        List<String> result = myList.stream().filter(s -> s.startsWith("C") || s.startsWith("c")).sorted().collect(Collectors.toList());
-        //System.out.println(result);
+    public void filterOutList(List<String> myList) {
+        List<String> result = myList.stream()
+                .filter(s -> s.startsWith("C") || s.startsWith("c"))
+                .sorted()
+                .collect(Collectors.toList());
     }
 
-    public void listProduct(List<Integer> list1 , List<Integer> list2)
-    {
-        List<Integer> resultingList =  IntStream.rangeClosed(0,min(list1.size(),list2.size())-1).map(index -> list1.get(index)*list2.get(index)).boxed().collect(Collectors.toList());
-        //System.out.println(resultingList);
+    public void listProduct(List<Integer> list1, List<Integer> list2) {
+        List<Integer> resultingList = IntStream.rangeClosed(0, min(list1.size(), list2.size()) - 1)
+                .map(index -> list1.get(index) * list2.get(index))
+                .boxed()
+                .collect(Collectors.toList());
     }
 
 }
